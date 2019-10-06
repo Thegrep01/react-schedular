e8d0d354254abaf24e848fdf037fca673f39ce52
“thegrep01” <den.belotserkovets@gmail.com>
2019-10-06 20:01:32 +0300

    update;

--- src/common/components/clock/clock.tsx
+++ src/common/components/clock/clock.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { formatDay, formatTime } from 'src/common/utils/formatters';
+import { Typography } from '@material-ui/core';
 
 export default function Clock() {
     const [time, setTime] = useState(getTime());
@@ -11,11 +12,11 @@ export default function Clock() {
         return () => clearInterval(intervalId);
     }, [time]);
 
-    return <>{time}</>;
+    return <Typography>{time}</Typography>;
 }
 
 function getTime(): string {
     const date: Date = new Date();
     const day: string = formatDay(date.getDay());
-    return `${day} ${formatTime(date.getHours().toString())} : ${formatTime(date.getMinutes().toString())}`;
+    return `${day} ${formatTime(date.getHours().toString())}:${formatTime(date.getMinutes().toString())}`;
 }
