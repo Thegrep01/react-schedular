c3237b220a72ae5eff0b7c573630ef0200b57769
“thegrep01” <den.belotserkovets@gmail.com>
2019-10-06 20:10:38 +0300

    update test

--- src/common/components/clock/clock.test.tsx
+++ src/common/components/clock/clock.test.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import Clock from './clock';
+import ReactDOM from 'react-dom';
 
-let realDate;
 
 describe('Test clock component', () => {
     it('show valid time', () => {
-        const currentDate = new Date('2019-05-14T11:01:58.135Z');
-        realDate = Date;
-        // @ts-ignore
-        global.Date = class extends Date {
-            public constructor(date) {
-                if (date) {
-                    super(date);
-                }
-                return currentDate;
-            }
-        };
-        const clock = renderer.create(<Clock></Clock>).toJSON();
-        expect(clock).toMatchSnapshot();
-        global.Date = realDate;
+        const div = document.createElement('div');
+        ReactDOM.render(<Clock />, div);
+        ReactDOM.unmountComponentAtNode(div);
     });
 });
-// 2016-06-20T12:08:10.000Z
+
